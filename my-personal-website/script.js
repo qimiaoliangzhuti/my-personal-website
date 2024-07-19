0d33b38f469cf94cf70660b13a07914ea49d1eec
qimiaoliangzhuti <3259721624@qq.com>
2024-07-20 04:04:03 +0800

    Add files via upload

---
+++ my-personal-website/script.js
@@ -0,0 +1,5 @@
+// script.js 文件内容 (这是一个可选的JavaScript文件，可根据需求自定义函数或添加交互功能)
+document.addEventListener('DOMContentLoaded', (event) => {
+    // 可以在这里添加一些JavaScript交互代码
+    console.log('DOM fully loaded and parsed');
+});
